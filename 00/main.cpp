ad2cbc7bb55d9907e7e25f45d7e311abb0d3cbaf
vladovinkin@book <vladovinkin@gmail.com>
2023-09-09 00:30:17 +0300

    Added A+B program

---
+++ 00/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+	int a = 0;
+	int b = 0;
+	std::cout << "enter two numbers:" << std::endl;
+	std::cin >> a >> b;
+	std::cout << "sum: " << (a + b) << std::endl;
+}
