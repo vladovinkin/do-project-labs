eafa3aadbf4feddc9e7e19d6a417cf1c2bb90cfc
vladovinkin@book <vladovinkin@gmail.com>
2023-09-28 20:09:40 +0300

    Added sinusoid motion

---
+++ sfml.2/02/main.cpp
@@ -0,0 +1,57 @@
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+#include <cmath>
+
+constexpr unsigned WINDOW_WIDTH = 800;
+constexpr unsigned WINDOW_HEIGHT = 600;
+
+void processEvent(sf::RenderWindow &window);
+
+int main()
+{
+    constexpr float BALL_SIZE = 40;
+
+    sf::RenderWindow window(sf::VideoMode({WINDOW_WIDTH, WINDOW_HEIGHT}), "Wave Moving Ball");
+    sf::Clock clock;
+
+    const sf::Vector2f position = { 10, 350 };
+
+    sf::CircleShape ball(BALL_SIZE);
+    ball.setFillColor(sf::Color(0xFF, 0xFF, 0xFF));
+
+    while (window.isOpen())
+    {
+        processEvent(window);   
+        
+        constexpr float speedX = 100.f;
+        constexpr float amplitudeY = 80.f;
+        constexpr float periodY = 2;
+
+        const float time = clock.getElapsedTime().asSeconds();
+        const float wavePhase = time * float(2 *  M_PI);
+        const float x = speedX * time;
+        const float y = amplitudeY * std::sin(wavePhase / periodY);
+        const sf::Vector2f offset = { x, y };
+        
+        ball.setPosition(position + offset);
+        
+        window.clear();
+        window.draw(ball);
+        window.display();
+    }
+
+    return 0;
+}
+
+void processEvent(sf::RenderWindow &window)
+{
+    sf::Event event;
+    while (window.pollEvent(event))
+    {
+        if (event.type == sf::Event::Closed)
+        {
+            window.close();
+        }
+    }
+}
