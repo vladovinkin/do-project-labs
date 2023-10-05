d592bf85de125b755743220f1a3deae05dc48abd
vladovinkin@book <vladovinkin@gmail.com>
2023-10-05 21:13:47 +0300

    print mouse events functions style

--- sfml.3/02/main.cpp
+++ sfml.3/02/main.cpp
@@ -1,40 +1,54 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 
+void pollEvents(sf::RenderWindow &window);
+void redrawFrame(sf::RenderWindow &window);
+
 int main()
 {
     constexpr unsigned WINDOW_WIDTH = 800;
     constexpr unsigned WINDOW_HEIGHT = 600;
 
-    sf::RenderWindow window(sf::VideoMode({ WINDOW_WIDTH, WINDOW_HEIGHT}), "Prints mouse events to terminal");
+    sf::RenderWindow window(
+        sf::VideoMode({WINDOW_WIDTH, WINDOW_HEIGHT}),
+        "Prints mouse events to terminal");
 
     while (window.isOpen())
     {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            switch (event.type)
-            {
-                case sf::Event::Closed:
-                    window.close();
-                    break;
-                case sf::Event::MouseButtonPressed:
-                    std::cout << "mouse pressed,"
-                              << " x=" << event.mouseButton.x
-                              << " y=" << event.mouseButton.y
-                              << std::endl;
-                    break;
-                case sf::Event::MouseButtonReleased:
-                    std::cout << "mouse pressed,"
-                            << " x=" << event.mouseButton.x
-                            << " y=" << event.mouseButton.y
-                            << std::endl;
-                    break;
-            }
-        }
-
-        window.clear();
-        window.display();
+        pollEvents(window);
+        redrawFrame(window);
     }
     return 0;
+}
+
+void pollEvents(sf::RenderWindow &window)
+{
+    sf::Event event;
+    while (window.pollEvent(event))
+    {
+        switch (event.type)
+        {
+        case sf::Event::Closed:
+            window.close();
+            break;
+        case sf::Event::MouseButtonPressed:
+            std::cout << "mouse pressed,"
+                      << " x=" << event.mouseButton.x
+                      << " y=" << event.mouseButton.y
+                      << std::endl;
+            break;
+        case sf::Event::MouseButtonReleased:
+            std::cout << "mouse pressed,"
+                      << " x=" << event.mouseButton.x
+                      << " y=" << event.mouseButton.y
+                      << std::endl;
+            break;
+        }
+    }
+}
+
+void redrawFrame(sf::RenderWindow &window)
+{
+    window.clear();
+    window.display();
 }
