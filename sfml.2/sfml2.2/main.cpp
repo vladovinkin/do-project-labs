d85a75a137dfd4ac7d46dc3f740cff6091480d04
vladovinkin@book <vladovinkin@gmail.com>
2023-10-05 20:56:03 +0300

    polar rose moving with delta time

--- sfml.2/sfml2.2/main.cpp
+++ sfml.2/sfml2.2/main.cpp
@@ -11,8 +11,11 @@ int main()
 {
     constexpr int pointCountShape = 200;
     constexpr int pointCountPath = 200;
+    constexpr float speed = 100.f;
 
     const float pathRadius = 105.f;
+    float position = 0;
+    sf::Clock clock;
 
     sf::ContextSettings settings;
     settings.antialiasingLevel = 8;
@@ -30,33 +33,31 @@ int main()
 
         sf::ConvexShape ellipse;
         ellipse.setFillColor(sf::Color(0xFF, 0x00, 0xFF));
+        const float dt = clock.restart().asSeconds();
 
-        for(int pointPathNo = 0; pointPathNo < pointCountPath; pointPathNo++)
+        position += speed * dt;
+        float angle = -float(2 * M_PI * position) / float(360);
+        sf::Vector2f point = {
+            pathRadius * std::sin(angle),
+            pathRadius * std::cos(angle)
+        };
+
+        ellipse.setPosition({point.x + 400, point.y + 300});
+        ellipse.setPointCount(pointCountShape);
+        for(int pointShapeNo = 0; pointShapeNo < pointCountShape; ++pointShapeNo)
         {
-            float angle = -float(2 * M_PI * pointPathNo) / float(pointCountPath);
+            float angle = float(2 * M_PI * pointShapeNo) / float(pointCountShape);
+            float ellipseRadius = 200 * std::sin(6 * angle);
             sf::Vector2f point = {
-                pathRadius * std::sin(angle),
-                pathRadius * std::cos(angle)
+                ellipseRadius * std::sin(angle),
+                ellipseRadius * std::cos(angle),
             };
-
-            ellipse.setPosition({point.x + 400, point.y + 300});
-            ellipse.setPointCount(pointCountShape);
-            for(int pointShapeNo = 0; pointShapeNo < pointCountShape; ++pointShapeNo)
-            {
-                float angle = float(2 * M_PI * pointShapeNo) / float(pointCountShape);
-                float ellipseRadius = 200 * std::sin(6 * angle);
-                sf::Vector2f point = {
-                    ellipseRadius * std::sin(angle),
-                    ellipseRadius * std::cos(angle),
-                };
-                ellipse.setPoint(pointShapeNo, point);
-            }
-           
-            window.clear();            
-            window.draw(ellipse);
-            window.display();
-            sf::sleep(sf::seconds(.01));
+            ellipse.setPoint(pointShapeNo, point);
         }
+        
+        window.clear();            
+        window.draw(ellipse);
+        window.display();
     }
 
     return 0;
